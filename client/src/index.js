518e62b11b43e8dfe66c49ff147c31b43eeb9085
KpoxaPy <kpoxapy@gmail.com>
2017-07-23 15:54:04 +0300

    Setup simple js client
    webpack, babel and react

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const div = document.getElementById('app');
+
+const App = (
+  <h1>Hello, world!</h1>
+);
+
+ReactDOM.render(App, div);
